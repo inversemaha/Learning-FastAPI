a27bbc8b6b460f31e2dbf700491cf3a93a1368d8
inversemaha <mahajur77@gmail.com>
2025-11-05 06:00:14 +0600

    feat: Implement frontend, API integration, and CORS middleware

--- FastAPI-002/main.py
+++ FastAPI-002/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import product
 
+# Initialize FastAPI app
 app = FastAPI()
+
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"]
+)
 
 @app.get("/")
 def greet():
